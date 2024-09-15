412acca13b852cd3ab2226128f8efc4e3d833415
Asadullah Azimi <asadullahazimi@Asadullahs-MacBook-Pro-46.local>
2024-09-15 18:59:15 -0400

    Multiplication operator overlaoded is completed

--- main.cpp
+++ main.cpp
@@ -27,14 +27,15 @@ private:
         while (temp != nullptr) {
             if (temp -> exp == exp) {
                 temp -> coeff += coeff;
-                delete newNode;
+                return;
             }
             temp = temp -> next;
         }
         if (head == nullptr) {
             head = newNode;
             tail = newNode;
-        } else {
+        }
+        else {
             tail -> next = newNode;
             tail = newNode;
         }
@@ -56,11 +57,11 @@ public:
 
     // Print the polynomial
     void print() const {
+        cout << "\n\nInside the print function \n\n";
         if (head == nullptr) {
             cout << "0" << endl;
             return;
         }
-
         Node* temp = head;
         bool first = true;
 
@@ -79,12 +80,11 @@ public:
         Polynomial result;
         Node* ptr1 = head;
         Node* ptr2 = right_hand_side_poly.head;
-        Node* last = result.head;
 
-        while (ptr1 != nullptr || ptr2 != nullptr) {
+        while (!(ptr1 == nullptr || ptr2 == nullptr)) {
             int coeff = 0, exp = 0;
             if (ptr1 -> exp == ptr2 -> exp) {
-                coeff = ptr1 -> coeff + ptr2->coeff;
+                coeff = ptr1 -> coeff + ptr2 -> coeff;
                 exp = ptr1 -> exp;
                 ptr1 = ptr1 -> next;
                 ptr2 = ptr2 -> next;
@@ -94,15 +94,7 @@ public:
                 exp = ptr1 -> exp;
                 ptr1 = ptr1 -> next;
             }
-            else if (ptr1 == nullptr) {
-                coeff = ptr2 -> coeff;
-                exp = ptr2 -> exp;
-                ptr2 = ptr2 -> next;
-            } else if (ptr2 == nullptr) {
-                coeff = ptr1 -> coeff;
-                exp = ptr1 -> exp;
-                ptr1 = ptr1 -> next;
-            } else {
+            else {
                 coeff = ptr2 -> coeff;
                 exp = ptr2 -> exp;
                 ptr2 = ptr2 -> next;
@@ -110,12 +102,13 @@ public:
 
             if (coeff != 0){
                 Node* newNode = new Node(coeff, exp);
-                if (last == nullptr){
+                if (result.head == nullptr){
                     result.head = newNode;
+                    result.tail = newNode;
                 }
                 else{
-                    last -> next = newNode;
-                    last = last -> next;
+                    result.tail -> next = newNode;
+                    result.tail = newNode;
                 }
             }
         }
@@ -123,10 +116,10 @@ public:
             return result;
         }
         else if (ptr1 == nullptr){
-            last -> next = ptr2;
+            result.tail -> next = ptr2;
         }
         else if (ptr2 == nullptr){
-            last -> next = ptr1;
+            result.tail -> next = ptr1;
         }
 
         return result;
@@ -138,28 +131,22 @@ public:
         Polynomial result;
         Node* ptr1 = head;
         Node* ptr2 = right_hand_side_poly.head;
-        Node* last = nullptr;
+        // Node* last = nullptr;
 
-        while (ptr1 != nullptr || ptr2 != nullptr) {
+        while (!(ptr1 == nullptr || ptr2 == nullptr)) {
             int coeff = 0, exp = 0;
-            if (ptr1 == nullptr) {
-                coeff = ptr2 -> coeff;
-                exp = ptr2 -> exp;
-                ptr2 = ptr2 -> next;
-            } else if (ptr2 == nullptr) {
-                coeff = ptr1 -> coeff;
-                exp = ptr1 -> exp;
-                ptr1 = ptr1 -> next;
-            } else if (ptr1 -> exp == ptr2 -> exp) {
-                coeff = ptr1 -> coeff - ptr2->coeff;
+            if (ptr1 -> exp == ptr2 -> exp) {
+                coeff = ptr1 -> coeff - ptr2 -> coeff;
                 exp = ptr1 -> exp;
                 ptr1 = ptr1 -> next;
                 ptr2 = ptr2 -> next;
-            } else if (ptr1 -> exp > ptr2 -> exp) {
+            } 
+            else if (ptr1 -> exp > ptr2 -> exp) {
                 coeff = ptr1 -> coeff;
                 exp = ptr1 -> exp;
                 ptr1 = ptr1 -> next;
-            } else {
+            } 
+            else {
                 coeff = ptr2 -> coeff;
                 exp = ptr2 -> exp;
                 ptr2 = ptr2 -> next;
@@ -167,65 +154,113 @@ public:
 
             if (coeff != 0){
                 Node* newNode = new Node(coeff, exp);
-                if (last == nullptr){
+                if (result.head == nullptr){
                     result.head = newNode;
+                    result.tail = newNode;
                 }
                 else{
-                    last -> next = newNode;
+                    result.tail -> next = newNode;
+                    result.tail = newNode;
                 }
-                last = newNode;
             }
         }
 
-        return result;
-    }
-
-
-
-    int raise_to(int coeff, int exp){
-        int result = 1;
-        while (exp > 0){
-            if (exp % 2 == 1){
-                result = result * coeff;
-            }
-            coeff = coeff * coeff;
-            exp = exp / 2;
-        }
-        return result;
-    }
-
-    Polynomial operator^(int exp){
-        Polynomial result;
-        Node* current = head;
-
-        if (exp == 0){
-            Node* newNode = new Node(1, 0);
-            result.head = newNode;
+        if (ptr1 == nullptr && ptr2 == nullptr){
             return result;
         }
-        
-        while(current != nullptr){
-            int coeff = current -> coeff;
-            coeff = raise_to(coeff, exp);
-            current -> coeff = coeff;
-            current -> exp = current -> exp * exp;
-            current = current -> next;
+        else if (ptr1 == nullptr){
+            result.tail -> next = ptr2;
         }
-        return *this;
+        else if (ptr2 == nullptr){
+            result.tail -> next = ptr1;
+        }
+
+        return result;
     }
 
 
     
-    // Polynomial operator*(const Polynomial right_hand_poly) const{
-    //     Polynomial result;
-    //     Node* current_left_poly = head;
-    //     Node* current_right_poly = right_hand_poly.head;
-    // }
+    Polynomial operator*(const Polynomial right_hand_poly) const{
+        Polynomial result;
+        Node* current_left_poly = head;
+        Node* current_right_poly = right_hand_poly.head;
+
+        while (current_left_poly != nullptr){
+            if (current_right_poly == nullptr){
+                current_left_poly = current_left_poly -> next;
+                current_right_poly = right_hand_poly.head;
+            }
+            else{
+                int coeff = 0, exp = 0;
+                coeff = current_left_poly -> coeff * current_right_poly -> coeff;
+                exp = current_left_poly -> exp + current_right_poly -> exp;
+                result.addTerm(coeff, exp);
+                current_right_poly = current_right_poly -> next;
+            }
+            
+        }
+     return result;
+    }
+
+
+
+    Polynomial operator^(int exp){
+        Polynomial result;
+        Polynomial base = *this;
+        Node* current = head;
+
+
+        if (exp == 0){
+            result.addTerm(1, 0);
+            return result;
+        }
+        result.addTerm(1, 0);
+        while(exp > 0){
+            if (exp % 2 == 1){
+                result = result * base;
+            }
+            base = base * base;
+            exp /= 2;
+        }
+        return result;
+    }
+
+
+    Polynomial operator%(const Polynomial& divisor) const {
+    
+        if (divisor.head == nullptr) {
+            throw invalid_argument("Division by zero polynomial.");
+        }
+
+        Polynomial dividend = *this;  // Copy of the dividend (the current polynomial)
+        Polynomial remainder = divisor; // The remainder is initially the dividend
+
+
+        while (remainder.head != nullptr && remainder.head->exp >= divisor.head->exp) {
+            // Find the leading term of the quotient: (leading term of remainder) / (leading term of divisor)
+            int leading_coeff = remainder.head->coeff / divisor.head->coeff;
+            int leading_exp = remainder.head->exp - divisor.head->exp;
+
+            // Create the leading term of the quotient
+            Polynomial leading_term;
+            leading_term.addTerm(leading_coeff, leading_exp);
+
+            // Subtract the product of the leading term and divisor from the remainder
+            Polynomial product = leading_term * divisor;  // Multiply leading term by divisor
+            remainder = remainder - product;  // Subtract the result from the remainder
+
+            // You could also add the leading_term to the quotient, but it's not necessary for remainder
+        }
+
+        // The remainder is returned, and it should have a degree less than the divisor
+        return remainder;
+    }
+
 
 
     // Parse polynomial from input
     void parseFromInput(int terms, istream& input) {
-        for (int i = 0; i < terms; ++i) {
+        for (int i = 0; i < terms; i++) {
             int coeff, exp;
             input >> coeff >> exp;
             addTerm(coeff, exp);
@@ -294,26 +329,18 @@ void processCommands() {
             Polynomial result = poly1 - poly2;
             result.print();
         }
-        // else if (operation == "MUL" || operation == "mul") {
-        //     int terms1, terms2;
-        //     iss >> terms1;
-        //     Polynomial poly1;
-        //     poly1.parseFromInput(terms1, iss);
+        else if (operation == "MUL" || operation == "mul") {
+            int terms1, terms2;
+            iss >> terms1;
+            Polynomial poly1;
+            poly1.parseFromInput(terms1, iss);
 
-        //     iss >> terms2;
-        //     Polynomial poly2;
-        //     poly2.parseFromInput(terms2, iss);
-        //     if (terms1 > terms2){
-        //         Polynomial result = poly1 * poly2;
-        //     }
-        //     else if (terms1 < terms2){
-        //         Polynomial result = poly2 * poly1;          
-        //     }
-        //    else{ 
-        //         Polynomial result = poly1 * poly2;
-        //     } 
-        //     result.print();
-        // }
+            iss >> terms2;
+            Polynomial poly2;
+            poly2.parseFromInput(terms2, iss);
+            Polynomial result = poly1 * poly2;
+            result.print();
+        }
         else if (operation == "EXP" || operation == "exp") {
             int terms = 0, n = -1;
             iss >> terms;
@@ -336,6 +363,9 @@ void processCommands() {
             iss >> terms2;
             Polynomial poly2;
             poly2.parseFromInput(terms2, iss);
+
+            Polynomial result = poly1 % poly2;
+            result.print();
 
         } 
         else if (operation == "QUIT" || operation == "quit") {
